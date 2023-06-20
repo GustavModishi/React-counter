6c16c183c3962e9affd01dcda1fa09b884a07fe3
Gustav Modishi <mamosadi.gustav@gmail.com>
2023-06-20 21:17:19 +0200

    first final product from a tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  let [count, setCount] = useState(0);
+
+  const handleIncr = () => {
+    setCount( count+1);
+  };
+
+  const handleDec = () => {
+    setCount(count-1);
+  };
+
+  const handleRes = () => {
+    setCount( 0 );
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h2>React Router 6 Tutorial</h2>
+        <button onClick={handleIncr}>increase</button>
+        <button onClick={handleDec}>Decrease</button>
+        <button onClick={handleRes}>Reset</button>
+
+        <h1>{count}</h1>
+ 
     </div>
+   
   );
 }
 
